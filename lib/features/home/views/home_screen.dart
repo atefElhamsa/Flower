2eb2af0eea778826b779c3566622ec696f6747eb
atefElhamsa <atefelhamsa79@gmail.com>
2024-04-28 03:11:54 +0300

    homescreen

--- lib/features/home/views/home_screen.dart
+++ lib/features/home/views/home_screen.dart
@@ -11,7 +11,6 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
