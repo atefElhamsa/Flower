6d7b2a2c1687dd56abaf9f6a04390ea0b406b0b6
atefElhamsa <atefelhamsa79@gmail.com>
2024-05-01 14:15:02 +0300

    not complete

--- lib/features/search_juice/views/widgets/juice_item_widget.dart
+++ lib/features/search_juice/views/widgets/juice_item_widget.dart
@@ -2,6 +2,7 @@
 
 import 'package:flower_app/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class JuiceItemWidget extends StatelessWidget {
   JuiceItemWidget({
@@ -20,7 +21,8 @@ class JuiceItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: MediaQuery.sizeOf(context).width * 0.04),
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.sizeOf(context).width * 0.04),
       child: Container(
         padding: EdgeInsets.all(MediaQuery.sizeOf(context).width * 0.04),
         decoration: BoxDecoration(
@@ -37,10 +39,43 @@ class JuiceItemWidget extends StatelessWidget {
           ),
         ),
         child: Stack(
-          alignment: Alignment.topRight,
           children: [
-            Image.asset(image),
-            Text(title),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: GoogleFonts.pangolin(
+                      textStyle: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: AppColors.brown,
+                        fontSize: MediaQuery.sizeOf(context).height * 0.025,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    description,
+                    style: GoogleFonts.roboto(
+                      textStyle: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: AppColors.brown.withOpacity(0.65),
+                        fontSize: MediaQuery.sizeOf(context).height * 0.02,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    price.toString(),
+                    style: GoogleFonts.roboto(),
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Image.asset(image),
+            ),
           ],
         ),
       ),
